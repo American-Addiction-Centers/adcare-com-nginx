7f99d176858076032d737cf8fe3d29a2976a6009
Phil Barera <phil.barera@gmail.com>
2021-07-08 10:52:39 -0700

    update for aws linux 2 env

---
+++ hooks/postdeploy/app_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod +x
+echo "HEY WERE CLEARING THAT CACHE"
+curl -X DELETE "https://staging.adcare.com/wordpress/cache/"
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
+# $WORDPRESS_URL https://admin.adcare.com/
