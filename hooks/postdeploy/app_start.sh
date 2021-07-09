a2630cfe0b5d5c8b1302598498a97fcda522a0f3
Phil Barera <phil.barera@gmail.com>
2021-07-09 14:31:14 -0700

    string replace

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "HEY CLEAR THE CACHE $WORDPRESS_URL"
+echo "${$WORDPRESS_URL/admin/''}"
 curl -A "aac-engineering" -X DELETE "https://staging.adcare.com/cache/fullpage/"
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
 # $WORDPRESS_URL https://admin.adcare.com/
