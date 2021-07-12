63dbb4af74148dc0a20a5bcbc24122d0f6a3363c
Phil Barera <phil.barera@gmail.com>
2021-07-12 10:41:31 -0700

    sleep after curl

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "CLEAR THAT CACHE $WORDPRESS_URL"
 echo "${WORDPRESS_URL/admin/}"
-curl -A "aac-engineering" -X DELETE "https://staging.adcare.com/cache/fullpage/"
+curl -X DELETE "https://staging.adcare.com/cache/fullpage/"
+sleep 3
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
 # $WORDPRESS_URL https://admin.adcare.com/
-
