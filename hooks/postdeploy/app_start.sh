8db5701c4b75aa102d35f214048951c4b3f6fd4a
Phil Barera <phil.barera@gmail.com>
2021-07-12 10:58:31 -0700

    test 2

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "CLEAR THAT CACHE $WORDPRESS_URL"
+echo "CLEAR THAT CACHES $WORDPRESS_URL"
 echo "${WORDPRESS_URL/admin/}"
-curl -X DELETE "https://staging.adcare.com/cache/fullpage/"
-sleep 3
+curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE "https://staging.adcare.com/cache/fullpage/"
+sleep 2
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
 # $WORDPRESS_URL https://admin.adcare.com/
