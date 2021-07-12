2fe8dd643f44a391b31993f076e8abbec95666c6
Phil Barera <phil.barera@gmail.com>
2021-07-12 14:52:46 -0700

    update echo

--- hooks/postdeploy/app_start.sh
+++ hooks/postdeploy/app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "CLEAR THAT CACHES $WORDPRESS_URL"
-echo "${WORDPRESS_URL/admin/}"
+echo "${WORDPRESS_URL/admin/}cache/fullpage/"
 curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 40 -X DELETE "https://staging.adcare.com/cache/fullpage/"
 sleep 2
 # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/platforms-linux-extend.html
